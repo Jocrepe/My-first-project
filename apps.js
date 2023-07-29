7caf9f5836bc36d591b913ab91c448cf7618e177
Jocrepe <ktpcrepe@gmail.com>
2023-07-29 22:01:55 +0700

    remake my first project
    rework css

--- apps.js
+++ apps.js
@@ -1,4 +1,5 @@
-function CalcTime(user_input_seconds) {
+function Calctime(user_input_seconds)
+{
     var seconds = user_input_seconds;
     var hours = user_input_seconds / 3600;
     var remaining_seconds1 = seconds % 3600;
@@ -8,11 +9,13 @@ function CalcTime(user_input_seconds) {
     return [hours, minutes, remaining_seconds2];
 }
 
-function display(element, value) {
+function display(element, value)
+{
+    document.getElementById('title3').innerHTML = "You exercise for"
     document.getElementById(element).innerHTML = Math.floor(value[0]) + " Hours " + Math.floor(value[1]) + " minutes " + value[2] + " seconds";
-    document.getElementById("title3").innerHTML = "คุณออกกำลังกายไปทั้งหมด"
 }
 
-function CalcTimeProgram(value) {
-    display("res", CalcTime(value));
+function CalctimeProgram(value)
+{
+    display('resdisplay', Calctime(value));
 }
